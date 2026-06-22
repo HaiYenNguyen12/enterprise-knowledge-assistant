c76ed967b06787729a38a60d58c759a7e1059369
Hai Yen Nguyen <nguyenthihaiyen120397@gmail.com>
2026-06-22 21:13:44 +0700

    create file document service

--- backend/app/services/document_service.py
+++ backend/app/services/document_service.py
@@ -1,18 +1,45 @@
-from backend.app.repositories.document_repository import DocumentRepository
+from uuid import uuid4
+from datetime import datetime
+from backend.app.services.qdrant_client import client
+from backend.app.services.pdf_service import extract_text_from_pdf
+from backend.app.services.chunk_service import split_text_into_chunks
+from backend.app.services.embedding_service import get_embeddings
 
 
 class DocumentService:
-    def __init__(self, document_repository):
+    def __init__(self, document_repository, qdrant_repository):
         self.document_repository = document_repository
+        self.qdrant_repository = qdrant_repository
 
     def upload_document(self, file):
-        return self.document_repository.create(file)
+      file_location = self._save_file(file)
+      document_id = str(uuid4())
+      self.document_repository.create_document(
+         document_id=document_id,
+         file_name=file.filename,
+         file_path=file_location,
+         file_size=file.size,
+         created_at=datetime.now()
+      )
 
-    def get_documents(self):
-        pass
+      text = extract_text_from_pdf(file_location)
+      chunks = split_text_into_chunks(text, chunk_size=1000, chunk_overlap=200)
+      points = []
+      for chunk in chunks:
+          embedding = get_embeddings(chunk)
+          points.append(
+              {
+                  "id": str(uuid4()),
+                  "vector": embedding.tolist(),
+                  "payload": {
+                      "chunk": chunk,
+                      "source_file": file.filename
+                  },
+              }
+          )
 
-    def update_document(self, document_id, document_data):
-        return self.document_repository.update(document_id, document_data)
 
-    def delete_document(self, document_id):
-        return self.document_repository.delete(document_id)
+
+
+
+ 
